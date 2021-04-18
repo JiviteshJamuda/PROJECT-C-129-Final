20b6aa8d4c006f5e33967ef80351bf5915a21bc3
JiviteshJamuda <jivitesh2006@gmail.com>
2021-04-18 20:15:18 +0530

    final

--- modifying_file.py
+++ modifying_file.py
@@ -33,7 +33,8 @@ for data in star_data:
         data[3] = float(data[3].split(' ')[0])
         # print(data[3])
     else:
-        data[3] = data[3].replace('?', '')
+        data[3] = data[3].replace('?', '0')
+        data[3] = float(data[3])
 
     if data[4] != '?':
         data[4] = data[4].replace('-', ' ')
@@ -43,7 +44,8 @@ for data in star_data:
         data[4] = float(data[4].split(' ')[0])
         # print(data[4])
     else:
-        data[4] = data[4].replace('?', '')
+        data[4] = data[4].replace('?', '0')
+        data[4] = float(data[4])
 
 with open('modified_file.csv', 'w') as f3:
     csv_writer = csv.writer(f3)
