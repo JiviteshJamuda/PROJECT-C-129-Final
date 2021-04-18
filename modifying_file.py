a8cf35979112e56de77cf65dc5d8959dd14142e4
JiviteshJamuda <jivitesh2006@gmail.com>
2021-04-18 14:30:52 +0530

    C-129-F

--- modifying_file.py
+++ modifying_file.py
@@ -11,13 +11,37 @@ with open('modified_file.csv', 'r') as f2:
     file2 = csv.reader(f2)
     for row in file2:
         data.append(row)
-print(data[0])
+# print(data[0])
 
 for i in range(0,len(data)):
     del data[i][1]
 
 headers = data[0]
 star_data = data[1:]
+
+for data in star_data:
+    if data[2] != '?':
+        data[2] = data[2].replace(',', '')
+        data[2] = data[2].replace('[', ' ')
+        data[2] = float(data[2].split(' ')[0])
+        # print(data[2])
+    
+    if data[3] != '?':
+        data[3] = data[3].replace('-', ' ')
+        data[3] = data[3].replace('â€“', ' ')
+        data[3] = data[3].replace('<', '')
+        data[3] = float(data[3].split(' ')[0])
+        data[3] = float(data[3]) * 0.000954588
+        # print(data[3])
+
+    if data[4] != '?':
+        data[4] = data[4].replace('-', ' ')
+        data[4] = data[4].replace('â€“', ' ')
+        data[4] = data[4].replace('Â´', ' ')
+        data[4] = data[4].replace(',', '')
+        data[4] = float(data[4].split(' ')[0])
+        data[4] = float(data[4]) * 0.102763
+        # print(data[4])
 
 with open('modified_file.csv', 'w') as f3:
     csv_writer = csv.writer(f3)
